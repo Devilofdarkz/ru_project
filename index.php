03613d99eeeb8d9c09b78da692301592a0a6004e
Kshitish sahoo <144921037+Devilofdarkz@users.noreply.github.com>
2025-04-28 20:33:23 +0530

    Delete index.php

--- index.php
+++
@@ -1,6 +0,0 @@
-<?php include 'header.php'; ?>
-<div class="jumbotron text-center">
-    <h1>Welcome to Rajendra University</h1>
-    <p>Question Repository</p>
-</div>
-<?php include 'footer.php'; ?>
